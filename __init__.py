d778c65155a920a71079f2c44a90feb625f81876
Saurabh Gaur <ghub@saurabhgaur.com>
2016-03-02 10:45:34 +0000

    Add hello template

--- __init__.py
+++ __init__.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
+
 @app.route("/")
-def hello():
-    return "Hello, I love Digital Ocean! Yahoo!"
+def home():
+    return "<h1 style='color:blue'>Hello There! You</h1>"
+
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
+
 
